8070f3c4e49c40c3d37d8257d2cb38764aa88a22
NathanDinh <nathan.n.dinh@gmail.com>
2017-12-15 08:49:40 +0000

    deploy heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB();
+//seedDB();
 
 
 //PASSPORT CONFIG
--- seeds.js
+++ seeds.js
@@ -4,66 +4,66 @@ var Comment   = require("./models/comment");
 
 
 var data = [
-    {
-        name:"Lake", 
-        image:"https://farm5.staticflickr.com/4081/4753265986_61aef197a0.jpg",
-        description:"VERY VERY NICE PLACE"
-    },
+    // {
+    //     name:"Lake", 
+    //     image:"https://farm5.staticflickr.com/4081/4753265986_61aef197a0.jpg",
+    //     description:"VERY VERY NICE PLACE"
+    // },
     
-    {
-        name:"Cloud",
-        image:"https://farm8.staticflickr.com/7289/16354308072_689d0d1a57.jpg",
-        description:"COld but fun"
-    },
+    // {
+    //     name:"Cloud",
+    //     image:"https://farm8.staticflickr.com/7289/16354308072_689d0d1a57.jpg",
+    //     description:"COld but fun"
+    // },
     
-    {
-        name:"Snow",
-        image:"https://farm1.staticflickr.com/112/316612921_f23683ca9d.jpg",
-        description:"COZY place"
-    }
+    // {
+    //     name:"Snow",
+    //     image:"https://farm1.staticflickr.com/112/316612921_f23683ca9d.jpg",
+    //     description:"COZY place"
+    // }
     
     
 ]
 
 function seedDB(){
-   //Remove all campgrounds
-//   Comment.remove({},function(err){
-//       if(err){
-//           console.log(err)
-//       } else {
-//           console.log("removed comment")
-//       }
-//   })
+//   Remove all campgrounds
+  Comment.remove({},function(err){
+      if(err){
+          console.log(err)
+      } else {
+          console.log("removed comment")
+      }
+  })
   Campground.remove({}, function(err){
-//         if(err){
-//             console.log(err);
-//         }
-//         console.log("removed campgrounds!");
-//         data.forEach(function(seed){
-//           Campground.create(seed, function(err,campground){
-//               if (err) {
-//                   console.log(err)
-//               } else {
-//                   console.log("added")
-//                   //create comment
-//                   Comment.create(
-//                       {
-//                         text:"i love this place",
-//                         author:"Nathan"
-//                   }, function(err,comment){
-//                       if(err){
-//                           console.log(err)
-//                       } else {
-//                         campground.comments.push(comment);
-//                         campground.save();
-//                         console.log("comments created")
-//                       }
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        data.forEach(function(seed){
+          Campground.create(seed, function(err,campground){
+              if (err) {
+                  console.log(err)
+              } else {
+                  console.log("added")
+                  //create comment
+                  Comment.create(
+                      {
+                        text:"i love this place",
+                        author:"Nathan"
+                  }, function(err,comment){
+                      if(err){
+                          console.log(err)
+                      } else {
+                        campground.comments.push(comment);
+                        campground.save();
+                        console.log("comments created")
+                      }
                       
-//                   });
-//               }
-//           }) 
-//         });
-//     //add few camp grounds
+                  });
+              }
+          }) 
+        });
+    //add few camp grounds
         
 
     }); 
